1bf58b38ddc4b18d408bec7be3ed5c72fa42d8ca
DenisKozhin <deniskozhincr@hmail.com>
2025-05-13 00:29:04 +0700

    фикс хз какой 2

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,8 +2,7 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
-import { AuthState, useAuthStore } from '@/providers/auth-store'
-import { LoaderIcon } from 'lucide-react'
+import { AuthState } from '@/providers/auth-store'
 
 type RouterContext = {
 	authentication: AuthState
@@ -14,18 +13,16 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 })
 
 function RootComponent() {
-	const authentication = useAuthStore()
-
 	return (
 		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
 			<SidebarProvider>
-				{authentication.isLoading && authentication.isAuth ? (
+				{/* {authentication.isLoading && authentication.isAuth ? (
 					<div className='flex h-[100dvh] w-full justify-center items-center'>
 						<LoaderIcon size={32} className='animate-spin' />
 					</div>
 				) : (
-					<Outlet />
-				)}
+				)} */}
+				<Outlet />
 				<Toaster />
 			</SidebarProvider>
 		</ThemeProvider>
