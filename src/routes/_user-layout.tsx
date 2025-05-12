9a4a361ba9e61f78310475ee5c47ab230e8b4a5a
DenisKozhin <deniskozhincr@hmail.com>
2025-05-13 00:40:40 +0700

    фикс хз какой 3

--- src/routes/_user-layout.tsx
+++ src/routes/_user-layout.tsx
@@ -1,41 +1,39 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { UserSidebar } from "@/components/user/user-sidebar";
-import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import { UserSidebar } from '@/components/user/user-sidebar'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/_user-layout")({
-  component: RouteComponent,
+export const Route = createFileRoute('/_user-layout')({
+	component: RouteComponent,
 
-  beforeLoad: async ({ context }) => {
-    const { authentication } = context;
+	beforeLoad: async ({ context }) => {
+		const { authentication } = context
 
-    console.log(authentication.user);
-
-    if (!authentication.isAuth) {
-      throw redirect({
-        to: "/login",
-      });
-    } else {
-      if (authentication.user?.role === "ADMIN") {
-        throw redirect({
-          to: "/requests",
-        });
-      }
-    }
-  },
-});
+		if (!authentication.isAuth) {
+			throw redirect({
+				to: '/login'
+			})
+		} else {
+			if (authentication.user?.role === 'ADMIN') {
+				throw redirect({
+					to: '/requests'
+				})
+			}
+		}
+	}
+})
 
 function RouteComponent() {
-  return (
-    <>
-      <UserSidebar />
+	return (
+		<>
+			<UserSidebar />
 
-      <main className="p-2 w-full flex flex-col h-[100dvh]">
-        <SidebarTrigger />
+			<main className='p-2 w-full flex flex-col h-[100dvh]'>
+				<SidebarTrigger />
 
-        <div className="flex flex-1 flex-col h-full overflow-y-auto">
-          <Outlet />
-        </div>
-      </main>
-    </>
-  );
+				<div className='flex flex-1 flex-col h-full overflow-y-auto'>
+					<Outlet />
+				</div>
+			</main>
+		</>
+	)
 }
