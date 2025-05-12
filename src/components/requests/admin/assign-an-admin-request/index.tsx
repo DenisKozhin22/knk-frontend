93ca54d877fff72836e715176d064c852b83dd47
DenisKozhin <deniskozhincr@hmail.com>
2025-05-12 22:35:28 +0700

    фикс

--- src/components/requests/admin/assign-an-admin-request/index.tsx
+++ src/components/requests/admin/assign-an-admin-request/index.tsx
@@ -1,66 +1,66 @@
 import {
-	AlertDialog,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogTrigger,
-	AlertDialogAction,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogTitle
-} from '@/components/ui/alert-dialog'
-import { Button } from '@/components/ui/button'
-import { useAssignAnAdminRequest } from '@/hooks'
-import { toast } from '@/hooks/use-toast'
-import { Request } from '@/types/models'
+  AlertDialog,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useAssignAnAdminRequest } from "@/hooks";
+import { toast } from "@/hooks/use-toast";
+import { Request } from "@/types/models";
 
 interface assignAnAdminRequestProps {
-	requestId: number
-	onSuccess?: (request: Request) => void
-	onCancel?: () => void
+  requestId: number;
+  onSuccess?: (request: Request) => void;
+  onCancel?: () => void;
 }
 
 export const AssignAnAdminRequest = ({
-	requestId,
-	onSuccess,
-	onCancel
+  requestId,
+  onSuccess,
+  onCancel,
 }: assignAnAdminRequestProps) => {
-	const { mutateAsync: assignRequest, isPending } = useAssignAnAdminRequest()
+  const { mutateAsync: assignRequest, isPending } = useAssignAnAdminRequest();
 
-	const onAssign = async () => {
-		try {
-			const request = await assignRequest(requestId.toString())
+  const onAssign = async () => {
+    try {
+      const request = await assignRequest(requestId);
 
-			onSuccess?.(request)
-		} catch (error) {
-			console.log(error)
-			toast({
-				title: 'Произошла ошибка'
-			})
-		}
-	}
-	return (
-		<AlertDialog>
-			<AlertDialogTrigger asChild>
-				<Button className='w-full'>Принять</Button>
-			</AlertDialogTrigger>
+      onSuccess?.(request);
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Произошла ошибка",
+      });
+    }
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="w-full">Принять</Button>
+      </AlertDialogTrigger>
 
-			<AlertDialogContent>
-				<AlertDialogHeader>
-					<AlertDialogTitle>Принять запрос?</AlertDialogTitle>
-					<AlertDialogDescription>
-						Вы уверены, что хотите принять этот запрос?
-					</AlertDialogDescription>
-				</AlertDialogHeader>
-				<AlertDialogFooter>
-					<AlertDialogCancel onClick={onCancel}>Нет</AlertDialogCancel>
-					<AlertDialogAction asChild>
-						<Button onClick={onAssign} disabled={isPending}>
-							Принять
-						</Button>
-					</AlertDialogAction>
-				</AlertDialogFooter>
-			</AlertDialogContent>
-		</AlertDialog>
-	)
-}
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Принять запрос?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Вы уверены, что хотите принять этот запрос?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={onCancel}>Нет</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button onClick={onAssign} disabled={isPending}>
+              Принять
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
